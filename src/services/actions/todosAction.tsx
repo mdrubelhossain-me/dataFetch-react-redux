3410532a838574909a795d63d50de9e376c662bc
mdrubelhossain-me <mdrubelhossain.cse@gmail.com>
2025-05-06 23:02:25 +0600

    fetch code added

---
+++ src/services/actions/todosAction.tsx
@@ -0,0 +1,29 @@
+//import axios from 'axios';
+import axios from "axios";
+import {
+  GET_TODOS_FAILURE,
+  GET_TODOS_REQUEST,
+  GET_TODOS_SUCCESS,
+} from "../constants/todosConstant";
+
+//action create
+export const getAllTodos = () => async (dispatch: any) => {
+  dispatch({ type: GET_TODOS_REQUEST });
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos"
+    );
+    dispatch({ type: GET_TODOS_SUCCESS, payload: response.data });
+  } catch (error) {
+    if (error instanceof Error) {
+      dispatch({ type: GET_TODOS_FAILURE, payload: error.message });
+    } else {
+      dispatch({
+        type: GET_TODOS_FAILURE,
+        payload: "An unknown error occurred",
+      });
+    }
+  }
+};
+
+export default getAllTodos;
